df1a84d02eacb60671276e34d7f7685a53825a71
Marco Antonio <marco_91-tc@hotmail.com>
2014-06-26 20:55:11 -0500

    Actualizando
    Modelo Libros

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,4 +1,18 @@
 class Book < ActiveRecord::Base
   belongs_to :provider
   belongs_to :category
+
+validates :isbn, :presence => true
+validates :isbn, :length {minimum: 2, maximum: 50,} => true
+validates :claveinterna, :presence => true
+validates :titulo, :presence => true
+validates :autor, :presence => true
+validates :editorial, :presence => true
+validates :stock, :presence => true
+validates :precio, :presence => true
+validates :provider_id, :presence => true
+validates :category_id, :presence => true
+
 end
+
+
