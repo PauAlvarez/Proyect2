57199ce76953def7bc154ede9fa290dd517a9604
Marco Antonio <marco_91-tc@hotmail.com>
2014-06-26 21:59:52 -0500

    Revert "Validando"
    This reverts commit 2a826bf9489c0933616ed493ea71c74808290219.

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,7 +1,4 @@
 class Category < ActiveRecord::Base
   has_many :books
 
-  validates :nombreCategoria, :presence => true
-  validates :descripcionCategoria, :presence => true
-
 end
--- app/models/provider.rb
+++ app/models/provider.rb
@@ -1,6 +1,2 @@
 class Provider < ActiveRecord::Base
-has_many :books
-
-  validates :NombreProvider, :presence => true
-  validates :direccionProvider, :presence => true
 end
