070e2bab2bdb9785b362974f8338c5fc7b503adc
PauAlvarez <seypau@live.com.mx>
2014-07-09 23:49:13 -0500

    Revert "Libros"

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,6 +1,2 @@
 class Category < ActiveRecord::Base
-
- validates :nombreCategoria, :presence => { message: "Introduzca un nombre" }
- validates :nombreCategoria, :uniqueness => { :message => "La categoria ya esta registrada" }
- validates :descripcionCategoria, :presence => { message: "No deje en blanco el campo" }
 end
